b813a2ba6ca3f7662a24d6d4fb7cfd5acad0dde7
jimbucktoo <jamesliang.g@gmail.com>
2019-03-31 19:42:08 -0600

    critical correction

--- arc/ViewController.swift
+++ arc/ViewController.swift
@@ -41,9 +41,9 @@ class ViewController: UIViewController {
     }
     
     func generateRandomColor() -> UIColor {
-        let hue : CGFloat = CGFloat(arc4random() % 256) / 256 // use 256 to get full range from 0.0 to 1.0
-        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from white
-        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5 // from 0.5 to 1.0 to stay away from black
+        let hue : CGFloat = CGFloat(arc4random() % 256) / 256
+        let saturation : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
+        let brightness : CGFloat = CGFloat(arc4random() % 128) / 256 + 0.5
         
         return UIColor(hue: hue, saturation: saturation, brightness: brightness, alpha: 1)
     }
